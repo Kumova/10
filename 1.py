b6e2685bd32dedafd47cf01f1c53e712da4d1ec6
Daria <you@example.com>
2022-02-27 21:51:59 +0300

    ___

--- 1.py
+++ 1.py
@@ -4,11 +4,22 @@ import requests
 
 TOKEN = "2619421814940190"
 
+class Person:
+     def name_request():
+        url = " https://superheroapi.com"
+        params = {
+           {"id" : 332, 'name': 'Hulk'},
+           {"id" : 149, 'name': 'Captain America'}, 
+           {"id" : 655, 'name': 'Thanos'}
+        }
+        response = requests.get_search_name(url, params=params, token=TOKEN)
+        pprint(response)
 
-def name_request():
-    url = " https://superheroapi.com"
-    params = {"id" : 332, "id" : 149, "id" : 655}
-    headers = {"Authorization": "secret - token - 2619421814940190"}
-    response = requests.get_name(url, params=params, headers=headers)
-    pprint(response)
+     def get_intelligence_person(self):
+        url = "https://superheroapi.com/api/access-token/search/name"
+        response = requests.get(url, token=TOKEN)
+        return response.json()
 
+if __name__ == '__main__':
+    person = Person()
+    pprint(person.get_intelligence_person())
